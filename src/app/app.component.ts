db3a25951d054b8bcfbc0ccafb3407ba7d6fb4ac
Nawanshu Choudhary <nawanshu@Nawanshus-MacBook-Air.local>
2020-02-12 09:17:21 +0530

    Repo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root-two',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
